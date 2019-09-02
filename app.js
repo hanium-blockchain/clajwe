b2da89e3f4bfb0740e7cdcea45cc0da0832effe4
leean <leeeeean91@gmail.com>
2019-09-02 19:53:46 +0900

    Update routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+//session
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
