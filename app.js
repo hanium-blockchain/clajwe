433b124caf218865ced89cc5efbf662b22a0eb89
leean <leeeeean91@gmail.com>
2019-09-02 19:27:47 +0900

    Create routers + Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var registersRouter = require('./routes/registers');
+var evaluationsRouter = require('./routes/evaluations');
+var investmentsRouter = require('./routes/investments');
+var managersRouter = require('./routes/managers');
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/registers', registersRouter);
+app.use('/evaluations', evaluationsRouter);
+app.use('/investments', investmentsRouter);
+app.use('/007', managersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
