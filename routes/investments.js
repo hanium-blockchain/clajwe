d13ae0f081813247f5874e7f23bcfa1431f9b74a
ChoiEunji0114 <cheunji8209@naver.com>
2019-09-03 16:04:24 +0900

    Create detail_context

--- routes/investments.js
+++ routes/investments.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/detail', function(req, res, next){
+    const asset = {
+        name : '아파아파트',
+        number: '112233',
+        address: '서울시 강남구 어쩌구',
+        area: 11223333,
+        complete_date: '2010-01-01',
+        description: '주변에 편의시설이 많음. 지하철 역에서 10분거리. 화장실 ',
+        end_date: '2020-05-05',
+    }
+    
+    res.render('detail/detail_context', {asset: asset});
+});
 
 
 module.exports = router;
--- routes/registers.js
+++ routes/registers.js
@@ -13,16 +13,43 @@ router.get('/new', function(req, res, next){
     res.render('detail/new_register');
 });
 
-function validateForm(form){
+function validateRegisterForm(form){
     var name = form.name || '';
     var number = form.number || 0;
     var address = form.address || '';
     var area = form.area || 0;
     var complete_date = form.complete-date-year || '';
+    var description = form.description || '';
+    var end_date = form.end-date-year || '';
+
+    if(!name){ return 'Name is required'; }
+    if(!number) { return 'Number is required'; }
+    if(!address) { return 'Address is required'; }
+    if(!area) { return 'Area is required'; }
+    if(!complete_date) { return 'Complete-date is required'; }
+    if(!description) { return 'Description is required'; }
+    if(!end_date) { return 'End-date is required'; }
+    
+    return null;
 }
 
 router.post('/request_register', (req, res, next) => {
-    var err = validateForm(req.body);
-})
+    var err = validateRegisterForm(req.body);
+    if(err){
+        req.flash('danger', err);
+        return res.redirect('back');
+    }
+    connection.connect(function(err){
+        if(err){
+            console.log(err);
+            console.log('connection error!!');
+        }
+        console.log('connection success!');
+
+    });
+    console.log(req.body);
+    req.flash('success', 'register success!');
+    res.redirect('back');
+});
 
 module.exports = router;
