26e00546e9f03bdb9ab62a6234beaee08d235d2c
jioo123 <bluephph12@gmail.com>
2019-09-18 21:35:03 +0900

    Update home

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@ var router = express.Router();
 var Data = require('./data')
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('user/mypage');
+// });
 router.get('/', function(req, res, next) {
-  res.render('user/mypage');
+  var myInvHead=[' ', '분류', '자산명', '가격']
+  const myInv = Data.myInv;
+  res.render('index',{check: 'myInv', list: myInvHead, myInv: myInv});
+  
 });
-
 router.get('/home', function(req, res, next) {
   var myInvHead=[' ', '분류', '자산명', '가격']
   const myInv = Data.myInv;
