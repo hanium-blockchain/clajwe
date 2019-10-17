da1cb0f846bc2b5ece665fd5a663e0527f675564
ChoiEunji0114 <cheunji8209@naver.com>
2019-10-17 15:19:12 +0900

    Upload jQuery

---
+++ public/javascripts/regi.js
@@ -0,0 +1,12 @@
+// $(function() {
+//     $('.test').click(function() {
+//       console.log('@@@@@@@@@@@ test @@@@@@@@@@@');
+//     });
+//   });
+
+
+$(document).ready(function() {
+    $("#test").click(function(){
+        console.log('@@@@@@@@@@@ test @@@@@@@@@@@');
+    }); 
+});
