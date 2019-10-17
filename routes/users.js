813ca19fb6189464e31082269bcfb158f802f048
jioo123 <bluephph12@gmail.com>
2019-10-17 18:22:57 +0900

    update

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('user/login');
 });
 
-
 router.get('/signin', (req, res, next) => {
-res.render('user/signin');
+  res.render('user/signin');
 });
 
 
