801cfc630e9acca09ee9d4b98b666e10ac24a8a1
ChoiEunji0114 <cheunji8209@naver.com>
2019-10-27 23:43:59 +0900

    Update evaluations.js

--- routes/evaluations.js
+++ routes/evaluations.js
@@ -9,19 +9,6 @@ const catchErrors = require('../lib/async-error');
 
 var Data = require('./data')
 
-
-// router.get('/detail/:id', catchErrors(async (req, res, next) => {
-//     const eval = await Evaluations.findById(req.params.id);
-//     console.log("??",eval);
-//     const user = await Users.findById(req.session.user.id);
-//     console.log("??????" , user);
-
-//     const asset = Data.asset;
-
-//     res.render('detail/eval_detail', {asset: asset, user: user});
-
-// }))
-
 router.get('/detail/:id', catchErrors(async (req,res, next) => {
     const asset = await Assets.findById(req.params.id);
     console.log("asset?? ", asset);
@@ -35,9 +22,9 @@ router.get('/detail/:id', catchErrors(async (req,res, next) => {
 
 router.get('/list', catchErrors(async (req, res, next) => {
     var evalHead = ['#', '분류', '자산명', '등록자', '등록일시']
-    const eval = await Evaluations.find({is_evaluate: false}).populate('user_id')
-    console.log(eval);
-    res.render('list/eval_invest_list', {title: '평가되지 않은 리스트',check: 'eval', list: evalHead, eval: eval});
+    const asset = await Assets.find({is_evaluate: false}).populate('user_id')
+    // console.log(eval);
+    res.render('list/eval_invest_list', {title: '평가되지 않은 리스트',check: 'eval', list: evalHead, eval: asset});
     
 }));
 
