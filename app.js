fbbd225480310c294428d96a14bbf8ead7244df5
Lee An <leeeeean91@gmail.com>
2019-10-27 16:25:16 +0900

    update session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
