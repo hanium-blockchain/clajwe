dac35b52ea632d3899a990070178628392eaaae3
ChoiEunji0114 <cheunji8209@naver.com>
2019-10-29 16:17:11 +0900

    Update date format

--- public/javascripts/eval.js
+++ public/javascripts/eval.js
@@ -3,9 +3,17 @@ $(function(){
        var evalValue = $('.evalInput').val();
        $('.coinLabel').text(evalValue*0.01);
    });
-
-
-  
-
 });
 
+$(document).ready(function(){
+
+    var sample = $('.sample_date').html();
+    var assetDate = new Date(sample);
+
+    var dd = assetDate.getDate();
+    var mm = assetDate.getMonth() + 1;
+    var yyyy = assetDate.getFullYear();
+
+    $('.sample_date').html(yyyy + '-' + mm + '-' + dd);
+
+});
